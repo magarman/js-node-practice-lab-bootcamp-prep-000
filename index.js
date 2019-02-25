d25547f77222f005afc125fcdbd308b945434378
Mariana Garman <garman.mariana@gmail.com>
2019-02-25 03:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Mariana"
-var height = "1.53"
+var height = 1.53
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+"1")
+console.log(height+1)
 
